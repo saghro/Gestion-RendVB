1929b5b0fa77f65dd3038b88baf56b213bb6c7f0
saghro <ayoubsaghro27@gmail.com>
2023-11-17 23:19:06 +0100

    create atts

--- src/main/java/ma/fsr/hospital/entities/Patient.java
+++ src/main/java/ma/fsr/hospital/entities/Patient.java
@@ -1,4 +1,10 @@
 package ma.fsr.hospital.entities;
 
+import java.util.Date;
+
 public class Patient {
+  private Long id;
+  private String nom;
+  private Date dateNaissance;
+  private boolean malade;
 }
