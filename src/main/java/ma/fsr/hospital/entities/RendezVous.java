5293bd1b6b1d0f91d501fe6e49535173f3934dd9
saghro <ayoubsaghro27@gmail.com>
2023-11-18 01:31:40 +0100

    create h2

--- src/main/java/ma/fsr/hospital/entities/RendezVous.java
+++ src/main/java/ma/fsr/hospital/entities/RendezVous.java
@@ -1,11 +1,23 @@
 package ma.fsr.hospital.entities;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class RendezVous {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Date date;
   private boolean annule;
+  @ManyToMany
   private Patient patient;
-
+  private StatusRDV status;
+  @ManyToMany
+  private Medecin medecin;
+  @OneToOne(mappedBy = "rendezVous")
+  private Consultation consultation;
 }
---
+++ src/main/java/ma/fsr/hospital/entities/StatusRDV.java
@@ -0,0 +1,7 @@
+package ma.fsr.hospital.entities;
+
+public enum StatusRDV {
+  PENDING,
+  CANCELED,
+  DONE,
+}
